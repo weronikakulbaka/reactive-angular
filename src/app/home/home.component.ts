6c625d9961d6d29866c96d3c9f1e8dcd0bdcf49f
Weronika <kulbaka.weronika@gmail.com>
2022-04-30 14:02:14 +0200

    smart vs presentation components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,8 +21,7 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog
+    private coursesService: CoursesService
   ) {
 
   }
@@ -42,20 +41,6 @@ export class HomeComponent implements OnInit {
       .pipe(
         map(courses => courses.filter(course => course.category === "ADVANCED"))
       );
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
 
 }
