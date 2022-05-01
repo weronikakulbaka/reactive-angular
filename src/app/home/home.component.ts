0a27a2b1df3e92d638a049874a7ec2a8bac73898
Weronika <kulbaka.weronika@gmail.com>
2022-05-01 14:54:33 +0200

    Reactive component interaction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Course, sortCoursesBySeqNo } from '../model/course';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { CoursesService } from '../services/courses.service';
-import { map } from 'rxjs/operators';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 
 const enum CourseCategory {
@@ -22,31 +25,16 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
   constructor(
-    private coursesService: CoursesService
+    private coursesStore: CoursesStore
   ) { }
-
-  static filterCourses(courses$: Observable<Course[]>, courseCategory: CourseCategory): Observable<Course[]> {
-    return courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category === courseCategory))
-      );
-  }
 
   ngOnInit(): void {
     this.reloadCourses();
   }
 
   reloadCourses(): void {
-    const courses$ = this.coursesService.loadAllCourses()
-      .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo))
-      );
-
-    this.beginnerCourses$ = HomeComponent.filterCourses(courses$, CourseCategory.BEGINNER);
-    this.advancedCourses$ = HomeComponent.filterCourses(courses$, CourseCategory.ADVANCED);
+    this.beginnerCourses$ = this.coursesStore.filterByCategory(CourseCategory.BEGINNER)
+    this.advancedCourses$ = this.coursesStore.filterByCategory(CourseCategory.ADVANCED)
   }
-
-
-
 
 }
---
+++ src/app/services/courses.store.ts
@@ -0,0 +1,77 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
+import { catchError, map, shareReplay, tap } from "rxjs/operators";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
+import { Course, sortCoursesBySeqNo } from "../model/course";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CoursesStore {
+
+    private subject = new BehaviorSubject<Course[]>([]);
+
+    courses$: Observable<Course[]> = this.subject.asObservable();
+
+    constructor(
+        private http: HttpClient,
+        private loading: LoadingService,
+        private messagesService: MessagesService,
+    ) {
+        this.loadAllCourses();
+    }
+
+    private loadAllCourses() {
+        const loadCourses$ = this.http.get<Course[]>('/api/courses')
+            .pipe(
+                map(response => response["payload"]),
+                catchError(err => {
+                    const message = "Could not load courses";
+                    this.messagesService.showErrors(message);
+                    console.log(message, err);
+                    return throwError(err);
+                }),
+                tap(courses => this.subject.next(courses))
+            );
+        
+        this.loading.showLoaderUntilCompleted(loadCourses$).subscribe();
+    }
+ 
+    filterByCategory(category: string): Observable<Course[]> {
+        return this.courses$
+            .pipe(
+                map(
+                  courses => courses.filter(course => course.category == category)
+                    .sort(sortCoursesBySeqNo)
+                )
+            )
+    }
+
+    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+        const courses = this.subject.getValue();
+        const index = courses.findIndex(course => course.id === courseId);
+
+        const newCourse: Course = {
+            ...courses[index],
+            ...changes
+        };
+        const newCourses: Course[] = courses.slice(0);
+        newCourses[index] = newCourse;
+
+        this.subject.next(newCourses);
+
+        return this.http.put(`/api/courses/${courseId}`, changes).pipe(
+            catchError(err => {
+                const message = "Could not save course";
+                console.log(message, err);
+                this.messagesService.showErrors(message);
+                return throwError(err);
+            }),
+            shareReplay(),
+        );
+
+    }
+}
+
